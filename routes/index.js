cbec56a6fca2f391824eabb25bee967bafbd26c4
Yudawardana94 <yuda.wardana40@gmail.com>
2022-03-01 17:46:18 +0700

    add check status route

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,13 @@ router.use("/pawangSinga", PawangSingaAppRouter);
 // router.get("/serverStatus", GlobalController.getServerStatus);
 // router.get("/search", GlobalController.search);
 router.use("/user", userRoutes);
+router.use("/s22", (_, res) => {
+  const object = {
+    status: "Connected",
+    message: "Server status active",
+  }
+  res.send(object)
+})
 router.get("/", (req, res) => {
   res.send("use localhost:3000/pawangSinga");
 });
